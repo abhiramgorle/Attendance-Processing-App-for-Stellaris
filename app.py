8e53afd96066262f9d9f2f928b3ea51f3eac090b
Abhiram Gorle <80030901+abhiramgorle@users.noreply.github.com>
2023-11-18 18:41:58 +0530

    Update app with the session numbers

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import csv
 from io import StringIO
 
-def process_csv(input_content, output_filename):
-    fields = ["Attendance Date", "Name", "Email", "Mobile", "Session-1", "Session-2"]
+def process_csv(input_content, output_filename,session_number):
+    
     rows = []
     rowss = []
 
@@ -12,19 +12,23 @@ def process_csv(input_content, output_filename):
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             rows.append(row)
-
+    x = session_number
     i = 0
     k = []
+    l = []
     for row in rows:
-        if i % 2 != 0:
-            for j in range(len(row)):
-                if j == 1:
-                    continue
-                else:
-                    k.append(row[j])
+        if i % x != 0:
+            k.append(row[-1])
         else:
             k.append(row[-1])
-            rowss.append(k)
+            l = row
+            l.pop(1)
+            l.pop(-1)
+            l[-1:-1] = k
+            j = l[-1]
+            l.pop(-1)
+            l.insert(3,j)
+            rowss.append(l)
             k = []
         i += 1
 
@@ -32,7 +36,7 @@ def process_csv(input_content, output_filename):
 
     with open(output_filename, 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(fields)
+        # csvwriter.writerow(fields)
         csvwriter.writerows(rowss)
 
     return rowss
@@ -43,24 +47,35 @@ st.title("Attendance Processing App for Suryam Sir")
 # File Upload
 uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
-if uploaded_file is not None:
+# Dropdown for number selection
+session_number = st.number_input("Enter number of sessions", min_value=1, max_value=10)
+
+
+
+if st.button("Process"):
     st.text("File Uploaded Successfully!")
 
     # Processing the file and getting the result
-    result_rows = process_csv(uploaded_file.read(), "result.csv")
-
+    result_rows = process_csv(uploaded_file.read(), "result.csv", session_number)
+    fields = ["Attendance Date", "Name", "Email", "Mobile"]
+    # sessions = []
+    for i in range(1, session_number+1):
+        session_name = f"Session-{i}"
+        fields.append(session_name)
     # Display the result
-    st.write("Processed Data:")
-    st.write(result_rows)
+    # st.write("Processed Data:")
+    # st.write(result_rows)
 
     # Download button for the result CSV
     csv_string = StringIO()
     csv_writer = csv.writer(csv_string)
+    csv_writer.writerow(fields)
     csv_writer.writerows(result_rows)
-    
+
     st.download_button(
         label="Download Processed CSV",
         data=csv_string.getvalue(),
         file_name="result.csv",
         key="download_button"
     )
+
